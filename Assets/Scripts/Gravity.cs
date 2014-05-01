ee0f066cc3bf31fbd3352bbdb1780e1a6765a433
flameiguana <flameiguana@gmail.com>
2014-05-01 00:34:08 -0700

    Made scene to visualize gravity on Mars.

---
+++ Assets/Scripts/Gravity.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Gravity : MonoBehaviour {
+	public float percentage;
+	private const float EARTH_GRAVITY = 9.81f;
+	// Use this for initialization
+
+
+	void Start () {
+		rigidbody.useGravity = false;
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		rigidbody.AddForce(-Vector3.up * Physics.gravity.magnitude *  percentage);
+	}
+}
