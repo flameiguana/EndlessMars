0641e13ca8de589322df43ce77dbbaa05380561a
flameiguana <flameiguana@gmail.com>
2014-05-03 14:06:51 -0700

    Added automatic rotation adjustment, disabled physics rotation. Fixed tags in controlscene

--- Assets/Scripts/Hover.cs
+++ Assets/Scripts/Hover.cs
@@ -13,10 +13,21 @@ public class Hover : MonoBehaviour {
 		layerMask = 1 << layer;
 	}
 
+	Vector3 targetUp;
+	float slerpRate;
+	//interpolate faster when connected to a platform
+	public float slerpRateSurface = 3f;
+	public float slerpRateStabilize = 1f;
+
+	public bool AutoRotate;
+
+
 	void FixedUpdate(){
 		RaycastHit info;
 		if(Physics.Raycast(transform.position, -Vector3.up, out info, HoverHeight, layerMask)){
-			transform.up = info.normal;
+			//if raycast is hitting, target up = normal of colliding surface
+			targetUp = info.normal;
+			slerpRate = slerpRateSurface;
 			//The desired height - distance to floor. Should be positive
 			float error = HoverHeight - info.distance;
 			//Scale force by amount of error. The more we're off, the faster we want to get to our desired height
@@ -27,5 +38,19 @@ public class Hover : MonoBehaviour {
 
 			rigidbody.AddForce(Vector3.up * (scaledForce - smooth));
 		}
+		else {
+			slerpRate = slerpRateStabilize;
+			//if raycast is not hitting, target up = world.up
+			targetUp = Vector3.up;
+		}
 	}
+
+	void LateUpdate(){
+		//Note that transform.up is being passed in as a parameter, so latest vector is always interpolated
+		//to target vector.
+		if(AutoRotate)
+			transform.up = Vector3.Slerp(transform.up, targetUp, Time.deltaTime * slerpRate);
+	}
+
+
 }
