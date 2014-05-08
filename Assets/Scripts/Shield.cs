f4ab86ddd95cc711498e23b20dfa172d8a2b4684
flameiguana <flameiguana@gmail.com>
2014-05-07 23:31:51 -0700

    Shield

---
+++ Assets/Scripts/Shield.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Shield : MonoBehaviour {
+
+	bool shieldingPlayer = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider collider){
+		if(collider.tag == "Player"){
+			gameObject.transform.parent = collider.gameObject.transform;
+			transform.localPosition = Vector3.zero;
+			shieldingPlayer = true;
+		}
+		else if(shieldingPlayer && collider.tag == "Obstacle"){
+			//Destroy obstacle and shield.
+			Destroy(collider.gameObject);
+			Destroy(gameObject);
+		}
+	}
+}
