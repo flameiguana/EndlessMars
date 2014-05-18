f225d488e4c943fd05821d4bee323744a1928b44
flameiguana <flameiguana@gmail.com>
2014-05-17 22:46:18 -0700

    Added credits and controls scenes

---
+++ Assets/ChangeSceneButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChangeSceneButton : MonoBehaviour {
+
+	public string level;
+
+	void OnPress(bool isDown){
+		if(!isDown){
+			Application.LoadLevel(level);
+		}
+	}
+}
