ba2dfd162f28c3281f073e29f70b6301543b2b19
flameiguana <flameiguana@gmail.com>
2014-05-19 21:21:26 -0700

    Stackable shields, up to 3.

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -8,6 +8,18 @@ public class CollisionHandler : MonoBehaviour
     public GameObject explosion;
     public AudioSource shieldPowerDown;
 
+	int shieldCount = 0;
+
+	Color[] colors = new Color[3];
+
+	void Awake(){
+
+		colors[0] = new Color(255f/255f, 255f/255f, 255f/255f);
+		colors[1] = new Color(37f/255f, 147f/255f, 221f/255f);
+		colors[2] = new Color(16f/255f, 0f/255f, 255f/255f);
+
+
+	}
 
 	public void GiveShield(Shield shield){
         if (!shielded)
@@ -16,11 +28,39 @@ public class CollisionHandler : MonoBehaviour
             shield.transform.localPosition = Vector3.zero;
             currentShield = shield;
             shielded = true;
+			shieldCount++;
         }
         else
         {
-            Destroy(shield);
+			Destroy(shield);
+			if(shieldCount < 3){
+				shieldCount++;
+				currentShield.renderer.material.SetColor("_Color", colors[shieldCount - 1]);
+			}
         }
+	}
+
+	void CollisionHandle(){
+		if(!shielded){
+			if(onDeath != null){
+                GameObject currentExplosion = (GameObject)Instantiate(explosion, transform.position, transform.rotation);
+				onDeath(gameObject);
+			}
+		}
+		else {
+			shieldPowerDown.Play();	
+			shieldCount--;
+
+			if(shieldCount == 0){
+				Destroy (currentShield.gameObject);
+				shielded = false;
+				currentShield = null;
+			}
+			else {
+				currentShield.renderer.material.SetColor("_Color", colors[shieldCount - 1]);
+			}
+
+		}
 	}
 
 	public delegate void DieAction(GameObject gameObject);
@@ -39,21 +79,7 @@ public class CollisionHandler : MonoBehaviour
 				Destroy(other.gameObject);
 				EnvironmentManager.instance.CreateObstacle();
 			}
-		
-
-
-			if(!shielded){
-				if(onDeath != null){
-                    GameObject currentExplosion = (GameObject)Instantiate(explosion, transform.position, transform.rotation);
-					onDeath(gameObject);
-				}
-			}
-			else {
-				Destroy (currentShield.gameObject);
-                shieldPowerDown.Play();
-				shielded = false;
-				currentShield = null;
-			}
+			CollisionHandle();
         }
 
 		if (other.gameObject.tag == "CrossBeam")
@@ -64,19 +90,7 @@ public class CollisionHandler : MonoBehaviour
 			EnvironmentManager.instance.crossBeam.Remove(other.gameObject);
 			Destroy(other.gameObject);
 			EnvironmentManager.instance.CreateObstacle();
-			
-			if(!shielded){
-				if(onDeath != null){
-					GameObject currentExplosion = (GameObject)Instantiate(explosion, transform.position, transform.rotation);
-					onDeath(gameObject);
-				}
-			}
-			else {
-                shieldPowerDown.Play();
-				Destroy (currentShield.gameObject);
-				shielded = false;
-				currentShield = null;
-			}
+			CollisionHandle();
 		}
 
     }
--- Assets/Scripts/Shield.cs
+++ Assets/Scripts/Shield.cs
@@ -4,16 +4,7 @@ using System.Collections;
 public class Shield : MonoBehaviour {
 
     public AudioSource powerUp;
-
-	// Use this for initialization
-	void Start () {
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
 
 	void OnTriggerEnter(Collider collider){
 		if(collider.tag == "Player"){
